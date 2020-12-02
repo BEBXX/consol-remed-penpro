b38fa3a9ed0e87f30d445312a28c91c875dc1899
Albert <71594935+BEBXX@users.noreply.github.com>
2020-12-02 22:41:48 +0700

    Delete soal debuging.py

--- jawaban consol/soal debuging.py
+++
@@ -1,8 +0,0 @@
-try:
-    a,b,c,d,e = map(int,input().split())
-    cond1 = (a <= b <= c <= d <= e)
-    cond2 = (a >= b >= c >= d >= e)
-    print(cond1 or cond2)
-
-except ValueError:
-    print("input tidak valid")
